d34853d120d4323257ae8bde6097cdcda4ce2a0b
RareMashiro <symon.belmont2015@yandex.ru>
2024-04-12 20:36:24 +0300

    Header, Footer and Layout are added!

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/header/component";
+import { Layout } from "./components/layout/component";
+import { Footer } from "./components/footer/component";
+
+export const App = () => {
+    return (
+        <>
+            <Header />
+            <Layout />
+            <Footer />
+        </>
+    )
+}
